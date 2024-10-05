79f9d9db3c76ec30de74f48a4a29d2d28ed769e9
kimjunhee <manuna530@gmail.com>
2024-10-05 21:13:55 +0900

    [feature] 조종 기능 버튼 위치 수정

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.capstone;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
 
         button1.setOnClickListener(v -> openCCTVActivity("rtsp://210.99.70.120:1935/live/cctv001.stream"));
-
-        button2.setOnClickListener(v -> openCCTVActivity("rtsp://210.99.70.120:1935/live/cctv031.stream"));
-
-        button3.setOnClickListener(v -> openCCTVActivity("rtsp://210.99.70.120:1935/live/cctv032.stream"));
-
-        button4.setOnClickListener(v -> openCCTVActivity("rtsp://210.99.70.120:1935/live/cctv033.stream"));
     }
 
     private void openCCTVActivity(String url) {
