c76875ee654a539da3a17ce6cc7941bb93ee802a
moonys2468 <51698451+moonys2468@users.noreply.github.com>
2024-10-08 21:17:51 +0900

    CCTV_MQTT

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.capstone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        Button button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(v -> openCCTVActivity("rtsp://210.99.70.120:1935/live/cctv001.stream"));
+    }
+
+    private void openCCTVActivity(String url) {
+        Intent intent = new Intent(MainActivity.this, CCTVActivity.class);
+        intent.putExtra("STREAM_URL", url);
+        startActivity(intent);
     }
 }
